43d3d8c3b615ca2454372fd6b427f8eb0ed3171e
Damian Hobson-Garcia <dhobsong@igel.co.jp>
2011-01-25 16:57:11 +0900

    vpu: avcdec: Use libmeram for IPMMUI access (IPMMUI is actually a part of MERAM)

--- src/vpu5/shvpu5_common_ipmmu.c
+++ src/vpu5/shvpu5_common_ipmmu.c
@@ -7,103 +7,89 @@
 #include <stdlib.h>
 #include "shvpu5_common_log.h"
 
-#define IPMMUI_BASE 0xFE951000
-#define IPMMUI_LEN 0x100
-#define IMCTR1 0x0
-#define IMCTR2 0x4
-#define IMPMBA0 0x90
-#define IMPMBD0 0xD0
+#include <meram/ipmmui.h>
+
 #define TB 5 /*log2 (block width) minimum value = 4*/
 #define VB 5 /*log2 (block height)*/
-#define IPMMUI_PAGE_SIZE 0x10 /*map all VPU accesses through one 64MB page*/
-#define IPMMUI_PAGE_MASK 0xFC000000 /*mask for 64MB page*/
-#define IMPPUI_ACCESS_BASE 0x80000000
-/*meram mapping data necessary set MERAM address mode*/
-#define MERAM_BASE 0xE8000000
-#define MERAM_LEN 0x8
-#define MEVCR1 0x4
 
-static int mem_fd;
-static void *ipmmu_base = NULL;
+IPMMUI *ipmmui;
+PMB *pmb0;
+unsigned long ipmmui_vaddr;
+unsigned long ipmmui_mask;
 
-static void
-write_ipmmu_reg(void *base, int off, unsigned long val)
-{
-        *(unsigned long *)(base + off) = val;
-}
-
-static void
-read_ipmmu_reg(void *base, int off, unsigned long *dest)
-{
-                *dest = *(unsigned long *)(base + off);
-}
 int
 init_ipmmu(int pmb, unsigned long phys_base, int log2_stride, int *align) {
-	void *meram_base;
-	if (!ipmmu_base) {
-		mem_fd = open ("/dev/mem", O_RDWR);
-		if (mem_fd < 0) {
-			perror("open");
-			return -1;
-		}
+	ipmmui = ipmmui_open();
+	IPMMUI_REG *reg;
+	int ipmmui_size;
+	int ipmmui_size_code;
 
-		ipmmu_base = mmap(NULL, IPMMUI_LEN, PROT_READ | PROT_WRITE,
-			MAP_SHARED, mem_fd, IPMMUI_BASE);
-		if (ipmmu_base == MAP_FAILED) {
-			perror("mmap");
-			close(mem_fd);
-			return -1;
-		}
-		/* Set the MERAM address range to 0xC0000000 - 0xDFFFFFFF
-		   so that it doesn't overlay the IPMMUI address range*/
-
-		meram_base = mmap(NULL, MERAM_LEN, PROT_READ | PROT_WRITE,
-			MAP_SHARED, mem_fd, MERAM_BASE);
-
-		if (meram_base == MAP_FAILED) {
-			loge("%s: MERAM mmap error", __FUNCTION__);
-			munmap(ipmmu_base, IPMMUI_LEN);
-			close(mem_fd);
-			return -1;
-		}
-		write_ipmmu_reg(meram_base, MEVCR1, (1 << 29));
-		munmap(meram_base, MERAM_LEN);
-	}
+	if (!ipmmui)
+		return -1;
 
 	if (log2_stride < TB)
 		return -1;
 
-	write_ipmmu_reg(ipmmu_base, IMPMBA0, IMPPUI_ACCESS_BASE | (1 << 8));
-	write_ipmmu_reg(ipmmu_base, IMPMBD0,
-                (phys_base & IPMMUI_PAGE_MASK) |
-                (1 << 8) | IPMMUI_PAGE_SIZE | ((VB - 1) << 20) |
+	if (ipmmui_get_vaddr(ipmmui, "vpu", &ipmmui_vaddr, &ipmmui_size) < 0)
+		return -1;
+	switch (ipmmui_size) {
+		case 16:
+			ipmmui_size_code = 0x0;
+			break;
+		case 64:
+			ipmmui_size_code = 0x10;
+			break;
+		case 128:
+			ipmmui_size_code = 0x80;
+			break;
+		case 256:
+			ipmmui_size_code = 0x90;
+			break;
+		default:
+			return -1;
+	}
+	ipmmui_mask = ~((ipmmui_size << 20) - 1);
+
+	pmb0 = ipmmui_lock_pmb(ipmmui, 0);
+	ipmmui_write_pmb(ipmmui, pmb0, IMPMBA, ipmmui_vaddr | (1 << 8));
+	ipmmui_write_pmb(ipmmui, pmb0, IMPMBD,
+                (phys_base & ipmmui_mask) |
+                (1 << 8) | ipmmui_size_code | ((VB - 1) << 20) |
                 ((log2_stride - TB - 1) << 16) | ((TB - 4) << 12)
                 | (1 << 9));
-	write_ipmmu_reg(ipmmu_base, IMCTR1, 2);
-	write_ipmmu_reg(ipmmu_base, IMCTR2, 1);
+
+	reg = ipmmui_lock_reg(ipmmui);
+	ipmmui_write_reg(ipmmui, reg, IMCTR1, 2);
+	ipmmui_write_reg(ipmmui, reg, IMCTR2, 1);
+	ipmmui_unlock_reg(ipmmui, reg);
 
 	*align = log2_stride + VB;
 	return 0;
 }
 void
 deinit_ipmmu() {
-	if (!ipmmu_base)
+	IPMMUI_REG *reg;
+	if (!ipmmui)
 		return;
 
-	write_ipmmu_reg(ipmmu_base, IMPMBA0, 0);
-	write_ipmmu_reg(ipmmu_base, IMPMBD0, 0);
-	write_ipmmu_reg(ipmmu_base, IMCTR2, 0);
-	write_ipmmu_reg(ipmmu_base, IMCTR1, 2);
+	ipmmui_write_pmb(ipmmui, pmb0, IMPMBA, 0);
+	ipmmui_write_pmb(ipmmui, pmb0, IMPMBD, 0);
+	ipmmui_unlock_pmb(ipmmui, pmb0);
 
-        munmap(ipmmu_base, IPMMUI_LEN);
-        close(mem_fd);
+	reg = ipmmui_lock_reg(ipmmui);
+	ipmmui_write_reg(ipmmui, reg, IMCTR2, 0);
+	ipmmui_write_reg(ipmmui, reg, IMCTR1, 2);
+	ipmmui_unlock_reg(ipmmui, reg);
+
+	ipmmui_close(ipmmui);
+
 }
 unsigned long
 phys_to_ipmmui(unsigned long address) {
-	return (address & ~IPMMUI_PAGE_MASK) | IMPPUI_ACCESS_BASE;
+	return (address & ~ipmmui_mask) | ipmmui_vaddr;
 }
 
 unsigned long
 ipmmui_to_phys(unsigned long ipmmu, unsigned long phys_base) {
-	return (ipmmu & ~IPMMUI_PAGE_MASK) | (phys_base & IPMMUI_PAGE_MASK);
+	return (ipmmu & ~ipmmui_mask) | (phys_base & ipmmui_mask);
 }
