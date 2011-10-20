bffc13c46692c81254c63668d441f80fe35ad28e
RSO Anonymous <not-signed-off-by@renesas.com>
2011-10-20 18:50:15 +0900

    vpu5ha: Don't store meta data for buffer with OMX_BUFFERFLAG_CODECCONFIG set
    The OMX_BUFFERFLAG_CODECCONFIG buffer contains only the SPS and PPS data, and
    no image data, so we shouldn't store metadata (timestamp etc) for it.

--- src/vpu5/shvpu5_avcdec_input.c
+++ src/vpu5/shvpu5_avcdec_input.c
@@ -237,7 +237,8 @@ mcvdec_uf_request_stream(MCVDEC_CONTEXT_T * context,
 	pBuf += 0x200;
 	for (i = 0; i < pPic->n_nals; i++) {
 		nal = pPic->pNal[i];
-		if (pBMI == NULL) {
+		if (pBMI == NULL &&
+			!(nal->pBuffer[0]->nFlags & OMX_BUFFERFLAG_CODECCONFIG)) {
 			logd("store buffer metadata\n");
 			pBMI = save_omx_buffer_metainfo(nal->pBuffer[0]);
 		}
