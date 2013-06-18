21ad292e3589607a91d6d54ce968119a095f052e
Damian Hobson-Garcia <dhobsong@igel.co.jp>
2013-06-18 13:46:11 +0900

    vpu5: omx: Set the coding type in sVideoParam
    The coding type (H264, MPEG4, VC1) needs to be set in both sPortParams
    and sVideoParams.  Make sure that this happens in all cases.

--- src/vpu5/shvpu5_decode_omx.c
+++ src/vpu5/shvpu5_decode_omx.c
@@ -235,6 +235,11 @@ shvpu_decode_Constructor(OMX_COMPONENTTYPE * pComponent,
 		sPortParam.format.video.eCompressionFormat =
 			shvpu_decode_Private->video_coding_type;
 
+	((omx_base_video_PortType *)(shvpu_decode_Private->
+			ports[OMX_BASE_FILTER_INPUTPORT_INDEX]))->
+			sVideoParam.eCompressionFormat =
+		shvpu_decode_Private->video_coding_type;
+
 	/** general configuration irrespective of any video formats
 	 * setting other parameters of shvpu_decode_private
 	 */
